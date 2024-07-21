f4a1179330f406242cd37c42e702952081a4933a
LizzBricce <bricce.e@aluno.ifsp.edu.br>
2024-07-21 18:59:08 -0300

    criando builder

--- src/model/Pizza.java
+++ src/model/Pizza.java
@@ -41,5 +41,44 @@ public class Pizza {
                 "Tipo de Massa: " + crust + "\n";
     }
 
+    public static class Builder {
+        private String cheese;
+        private List<String> toppings;
+        private SizeType size;
+        private CrustType crust;
+
+        public Builder() {
+            this.cheese = "mussarela";
+            this.toppings = new ArrayList<>();
+            this.size = SizeType.DEFAULT;
+            this.crust = CrustType.THIN;
+        }
+
+        public Builder cheese(String cheese) {
+            this.cheese = cheese;
+            return this;
+        }
+
+        public Builder addTopping(String topping) {
+            this.toppings.add(topping);
+            return this;
+        }
+
+        public Builder size(SizeType size) {
+            this.size = size;
+            return this;
+        }
+
+        public Builder crust(CrustType crust) {
+            this.crust = crust;
+            return this;
+        }
+
+        public Pizza build() {
+            return new Pizza(this.cheese, this.toppings, this.size, this.crust);
+        }
+
+    }
+
 
 }
